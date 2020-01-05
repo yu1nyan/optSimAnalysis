420fbc7a04b5a696d49a4a1d8ddcf30bfe8b80f9
awayui <yuiyuiawayui@gmail.com>
2020-01-05 23:10:22 +0900

    bug fix

--- const.h
+++ const.h
@@ -15,6 +15,10 @@ const string CubeGeometryTitleAround[] = { "upper left", "upper", "upper right",
 
 const string histNameZHitTime = "hittimez";
 
+const string histNameTrueOptical = "trueoptctrosstalk";
+const string histNameDeltaRay = "deltaray";
+const string histNameCherenkov = "fibercherenkov";
+
 const string histNameCubeIn = "cubeinpos";
 const string histNameCubeOut = "cubeoutpos";
 
